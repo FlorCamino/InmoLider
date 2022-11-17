b8e160c8919169db50e56347c4127ef98bff7c66
Florencia Camino <flopiis.1994@gmail.com>
2022-11-17 11:31:55 -0300

    'adding models'

--- app/Models/Propiedad.php
+++ app/Models/Propiedad.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Symfony\Component\HttpKernel\Fragment\EsiFragmentRenderer;
 
 class Propiedad extends Model
 {
@@ -11,8 +12,18 @@ class Propiedad extends Model
 
     protected $table = 'propiedad';
 
+    protected $fillable = [
+        'id', 'descripcion', 'fechaCreacion', 'direccion', 'barrio', 'CP',
+        'idPropietario', 'cantHab', 'cantBanios', 'estacionamiento', 'idCiudad',
+        'cantHab', 'cantBanios', 'aceptaMascotas', 'amoblado', 'idTipoTransaccion',
+        'idEstadoPropiedad', 'costo'
+    ];
+
     public function Ciudad()
     {
         return $this->belongsTo(Ciudad::class, 'idCiudad');
+        return $this->belongsTo(Periodo::class, 'idPeriodo');
+        return $this->belongsTo(EstadoPropiedad::class, 'idEstadoPropiedad');
+        return $this->belongsTo(TipoTransaccion::class, 'idTipoTransaccion');
     }
 }
---
+++ app/Models/TipoTransaccion.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TipoTransaccion extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'idTipoTransaccion', 'name', 'descripcion'
+    ];
+}
