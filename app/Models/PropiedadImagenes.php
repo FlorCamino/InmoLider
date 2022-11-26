e51454fcec4a6f7437a6761c3f2b7d3a681ba9c1
Florencia Camino <flopiis.1994@gmail.com>
2022-11-26 20:42:31 -0300

    Imagenes model

---
+++ app/Models/PropiedadImagenes.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PropiedadImagenes extends Model
+{
+    use HasFactory;
+    protected $table = 'propiedadImagenes';
+    public $timestamps = false;
+    protected $fillable = [
+        'id', 'idPropiedad', 'urlFoto', 'fechaDeCarga'
+    ];
+    protected $guarded = ['id'];
+    public function Propiedad()
+    {
+        return $this->belongsTo(Propiedad::class, 'idPropiedad');
+    }
+}
