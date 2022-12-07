209151233bc6a3b863c0f3d30a8fc1840fa7f291
Florencia Camino <flopiis.1994@gmail.com>
2022-12-06 21:11:53 -0300

    template nosotros finzalizada

--- resources/views/inicio/nosotros.blade.php
+++ resources/views/inicio/nosotros.blade.php
@@ -3,7 +3,6 @@
 @section('content')
 
     <body>
-
         <main>
             <div class="container nos-container">
                 <div id="myCarousel" class="carousel slide carousel-dark" data-bs-ride="carousel">
@@ -57,83 +56,50 @@
                         <span class="visually-hidden">Después</span>
                     </button>
                 </div>
-
-
-                <!-- START THE FEATURETTES -->
-
                 <hr>
-
-                <div class="row featurette-all">
-                    <div class="col-md-7 featurette">
-                        <h2 class="featurette-heading">InmoLider&copy;. <span class="text-muted">Cumpliendo
-                                sueños!</span></h2>
-                        <p class="lead">Somos una empresa transparente y sólida en la localidad de Río Grande,
-                            Tierra del Fuego, que cuenta con más de 40 años de experiencia en la actividad inmobiliaria.
-                            Su director y fundador, J. R. Lombardi junto a su equipo, trabajan arduamente para satisfacer
-                            las necesidades de sus clientes.
-                            Ofrece asesoramiento durante el proceso de tasación, compra, venta, construcción y
-                            administración
-                            de inmuebles, impulsándolos a generar siempre mayor rentabilidad en sus inversiones.
-                            Actualmente, nos estamos impulsando a mejorar nuestros servicios inviertiendo en nuevas
-                            tecnologías
-                            para que puedas acceder a nuestros servicios desde tu hogar.
-                        </p>
+                <div class="container nos-featurette">
+                    <div class="row featurette-all">
+                        <div class="col-md-7 featurette">
+                            <h2 class="featurette-heading">InmoLider&copy;. <span class="text-muted">Cumpliendo
+                                    sueños!</span></h2>
+                            <p class="lead">Somos una empresa transparente y sólida en la localidad de Río Grande,
+                                Tierra del Fuego, que cuenta con más de 40 años de experiencia en la actividad inmobiliaria.
+                                Su director y fundador, J. R. Lombardi junto a su equipo, trabajan arduamente para
+                                satisfacer
+                                las necesidades de sus clientes.
+                                Ofrece asesoramiento durante el proceso de tasación, compra, venta, construcción y
+                                administración
+                                de inmuebles, impulsándolos a generar siempre mayor rentabilidad en sus inversiones.
+                                Actualmente, nos estamos impulsando a mejorar nuestros servicios inviertiendo en nuevas
+                                tecnologías
+                                para que puedas acceder a nuestros servicios desde tu hogar.
+                            </p>
+                        </div>
+                        <div class="col-md-5">
+                            <img class="img-nos" role="img" src="{{ asset('img/img-rio-grande.jpg') }}" alt="logo">
+                        </div>
                     </div>
-                    <div class="col-md-5">
-                        <img class="img-nos" role="img" src="{{ asset('img/img-rio-grande.jpg') }}" alt="logo">
-
+                    <hr class="featurette-divider">
+                    <div class="row featurette">
+                        <div class="col-md-7 order-md-2">
+                            <h2 class="featurette-heading fw-normal lh-1">Río Grande<span class="text-muted"></span></h2>
+                            <p class="lead">Río Grande es una de las tres localidades en la
+                                Provincia de Tierra del Fuego Antartida e Islas del Atlantico Sur,
+                                que presenta una población de al menos 99 mil habitantes. Su nombre
+                                es debenido de el reconocido afluente austral Rio Grande hacia la costa
+                                del Mar Argentino.
+                                Su principal economía esta basada en la Industria tanto nacional como
+                                internacional.
+                                La misma es reconocida por la capital de la trucha, pero también como la
+                                capital de la vigilia por nuestras enorgullecedoras Islas Malvinas. </p>
+                        </div>
+                        <div class="col-md-5">
+                            <img class="img-nos" role="img" src="{{ asset('img/img-islas-rg.jpg') }}" alt="logo">
+                        </div>
                     </div>
+                    <hr class="featurette-divider">
                 </div>
-
-                <hr class="featurette-divider">
-
-                <div class="row featurette">
-                    <div class="col-md-7 order-md-2">
-                        <h2 class="featurette-heading fw-normal lh-1">Oh yeah, it’s that good. <span class="text-muted">See
-                                for yourself.</span></h2>
-                        <p class="lead">Another featurette? Of course. More placeholder content here to give you an
-                            idea
-                            of how this layout would work with some actual real-world content in place.</p>
-                    </div>
-                    <div class="col-md-5 order-md-1">
-                        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
-                            width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img"
-                            aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false">
-                            <title>Placeholder</title>
-                            <rect width="100%" height="100%" fill="#eee" /><text x="50%" y="50%"
-                                fill="#aaa" dy=".3em">500x500</text>
-                        </svg>
-
-                    </div>
-                </div>
-
-                <hr class="featurette-divider">
-
-                <div class="row featurette">
-                    <div class="col-md-7">
-                        <h2 class="featurette-heading fw-normal lh-1">And lastly, this one. <span
-                                class="text-muted">Checkmate.</span></h2>
-                        <p class="lead">And yes, this is the last block of representative placeholder content. Again,
-                            not
-                            really intended to be actually read, simply here to give you a better view of what this
-                            would
-                            look like with some actual content. Your content.</p>
-                    </div>
-                    <div class="col-md-5">
-                        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
-                            width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img"
-                            aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false">
-                            <title>Placeholder</title>
-                            <rect width="100%" height="100%" fill="#eee" /><text x="50%" y="50%"
-                                fill="#aaa" dy=".3em">500x500</text>
-                        </svg>
-
-                    </div>
-                </div>
-
-                <hr class="featurette-divider">
-
-                <!-- /END THE FEATURETTES -->
-
-            </div><!-- /.container -->
-        @endsection
+            </div>
+        </main>
+    </body>
+@endsection
