56e630c54a4abe75e71f14cf3788f40a5b9ac8a2
Florencia Camino <flopiis.1994@gmail.com>
2022-12-12 15:34:27 -0300

    espaciados en model de propiedadImagenes

--- app/Models/PropiedadImagenes.php
+++ app/Models/PropiedadImagenes.php
@@ -8,12 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class PropiedadImagenes extends Model
 {
     use HasFactory;
+
     protected $table = 'propiedadImagenes';
+
     public $timestamps = false;
+
     protected $fillable = [
         'id', 'idPropiedad', 'urlFoto', 'fechaDeCarga'
     ];
+
     protected $guarded = ['id'];
+
     public function Propiedad()
     {
         return $this->belongsTo(Propiedad::class, 'idPropiedad');
