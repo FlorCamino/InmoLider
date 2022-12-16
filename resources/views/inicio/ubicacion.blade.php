474048a86ee2081d2c6c0f0610e996ae61ddca4e
Vastayalover <116389999+Vastayalover@users.noreply.github.com>
2022-12-16 17:26:21 -0300

    Create ubicacion.blade.php

---
+++ resources/views/inicio/ubicacion.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.base_user')
+
+@section('content')
+
+    <body>
+        <main>
+            <hr>
+            <div class="container marketing">
+            <hr class="featurette-divider">
+                    <div class="row featurette">
+                        <div class="col-lg-4">  
+                        <div class="v-line1">
+                            <h3 class="featurette-heading fw-normal lh-1">Sede Río Grande<span class="text-muted"></span></h3>
+                            <p class="lead">Nuestra sede oficial de Rio Grande <hr>
+                                <!--podras acercarte presencialmente a realizar 
+                                cualquier consulta de interes o presentar tus quejas-->  </p>
+                            </div>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2357.0077387446163!2d-67.73087994879334!3d-53.789349810913535!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xbc4b16707ca18f61%3A0xaace43688bcd81a5!2sIslas%20Malvinas%201834%2C%20R%C3%ADo%20Grande%2C%20Tierra%20del%20Fuego!5e0!3m2!1ses-419!2sar!4v1671218677555!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+        </main>
+    </body>
+@endsection
