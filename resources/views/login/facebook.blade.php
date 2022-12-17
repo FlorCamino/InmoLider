d1fc8c4e95dfbb6d7fe15655ce73e44ab029910f
Vastayalover <116389999+Vastayalover@users.noreply.github.com>
2022-12-16 23:21:41 -0300

    Create facebook.blade.php

---
+++ resources/views/login/facebook.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.base_user')
+
+@section('content')
+
+    <body>
+        <main>
+            <hr>
+            <div class="container marketing">
+            <hr class="featurette-divider">
+                    <div class="row featurette">
+                        <div class="col-lg-4">  
+                        <div class="line1">
+                            <h3 class="featurette-heading fw-normal lh-1">No lo hicimos profe :(<span class="text-muted"></span></h3>
+                            </div>
+                    </div>
+                </div>
+        </main>
+    </body>
+@endsection
+
