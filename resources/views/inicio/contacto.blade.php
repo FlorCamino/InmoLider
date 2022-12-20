c5b814a9145fcfd4139f16ef4560ed2a5cd932c8
Florencia Camino <flopiis.1994@gmail.com>
2022-12-20 11:10:36 -0300

    estilos a contacto template

--- resources/views/inicio/contacto.blade.php
+++ resources/views/inicio/contacto.blade.php
@@ -3,55 +3,56 @@
 
 <div class="container marketing">
     <div class="row">
-        <div class="col-lg-4">
+        <div class="col-4">
             <img class="img-cont" role="img" src="{{ asset('img/Daniela.jpg') }}" alt="logo">
             <h2 class="name">Ramirez Daniela</h2>
             <p>Agente de bienes raíces con 5 años de experiencia, amable y predispuesta a formar parte de tu
                 sueño de una vivienda.</p>
-            <div class="col-4">
+            <div class="col-7">
                 <ul class="cont-list">
-                    <a href="https://wa.me/numero"><span class="iconify" data-icon="ic:baseline-whatsapp"></span>2964568475</a>
-                    <a href="mailto:ejemplo@gmail.com"><span class="iconify" 
-                            data-icon="material-symbols:alternate-email">tubbromi@yahoo.com</a>
-                    <a href="https://www.instagram.com/"><i class="fab fa-instagram"></i>@DaniRamirez</a>
-                    <a href="https://www.facebook.com/"><span class="iconify" data-icon="ic:baseline-facebook">@DaniRamirez</a>
+                    <li><a href="https://wa.me/numero">Link whatsapp</a></li>
+                    <li><a href="mailto:ejemplo@gmail.com">Link email</a></li>
+                    <li> <a href="https://www.instagram.com/">Link instagram</a></li>
+                    <li> <a href="https://www.facebook.com/">Link facebook</a></li>
                 </ul>
             </div>
         </div>
         <div class="v-line">
         </div>
-        <div class="col-lg-4">
+        <div class="col-4">
             <img class="img-cont" role="img" src="{{ asset('img/Pablo.jpg') }}" alt="logo">
             <h2 class="name"> &nbsp Perez Pablo</h2>
             <p>Mas de 2 años acompañandonos y brindando <br> la mejor calidad de atención al cliente.</p>
             <div class="col-7">
                 <ul class="cont-list">
-                    <li class="cont-datos"><span class="iconify" data-icon="ic:baseline-whatsapp"></span>+542964425678
+                    <li>
+                        <a href="https://wa.me/numero">Link whatsapp</a>
                     </li>
-                    <li class="cont-datos"><span class="iconify"
-                            data-icon="material-symbols:alternate-email"></span>pabloinmolider<br>@gmail.com
+                    <li> <a href="mailto:pabloinmolider@gmail.com"> Link email</a>
                     </li>
-                    <li class="cont-datos"><span class="iconify" data-icon="mdi:instagram"></span>pabloinmolider</li>
-                    <li class="cont-datos"><span class="iconify" data-icon="ic:baseline-facebook"></span>Pablo Perez
-                        InmoLider
+                    <li>
+                        <a href="https://www.instagram.com/">Link instagram</a>
+                    </li>
+                    <li> <a href="https://www.facebook.com/"> Link facebook
+                            InmoLider</a>
                     </li>
                 </ul>
             </div>
         </div>
-        <div class="v-line1">
+        <div class="v-1line">
         </div>
-        <div class="col-lg-4">
+        <div class="col-4">
             <img class="img-cont" role="img" src="{{ asset('img/Sonia.jpg') }}" alt="logo">
             <h2 class="name">Lombardi Sonia</h2>
             <p>Cuenta con más de 15 años de experiencia y el conocimiento para brindarte el mejor
                 asesoramiento
                 en bienes raices.</p>
-            <div class="col-4">
+            <div class="col-7">
                 <ul class="cont-list">
-                    <li class="cont-datos"><span class="iconify" data-icon="ic:baseline-whatsapp"></span>+542964645676
+                    <li>
+                        <a href="www.whatsapp.com">Link whatsapp</a>
                     </li>
-                    <li class="cont-datos"><span class="iconify"
-                            data-icon="material-symbols:alternate-email"></span>sonialombardi@gmail.com
+                    <li> <a href="meilto:sonialombardi@gmail.com">Link email</a>
                     </li>
                 </ul>
             </div>
